b929b023ef7e0b67c78127f46b97c1aed31dd78a
Daniel Castellano <daniel.castellanom@gmail.com>
2017-11-20 12:58:07 -0600

    Arreglada logica de Game para esperar a ambos jugadores

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,7 +7,7 @@ using UnityEngine.Networking;
 
 public class Game : MonoBehaviour {
 
-    // Use this for initialization
+    // Clase interna usada para manejar las sesiones.
     [Serializable]
     public class GameServer
     {
@@ -25,6 +25,7 @@ public class Game : MonoBehaviour {
         }
     }
 
+    
     public GameObject player1;
     public GameObject player2;
     TankScript enemy;
@@ -33,38 +34,34 @@ public class Game : MonoBehaviour {
 
     void Start () {
         gameBuilder();
-
-    }
-
-    // Update is called once per frame
-    void Update () {
-
+  
     }
 
     void gameBuilder()
     {
         var activeSession = createGameSession();
 
-        if(activeSession[0].gameId != null)
+        if(activeSession[0].gameState  == "Waiting")
         {
-            instantiatePlayers();
+            Debug.Log("Esperando jugadores");
+            //Instancia jugadores
+            //Envia jugadores
+            //Revisa de nuevo
+            //Inicia Juego
+            StartCoroutine(getSession(activeSession[0].gameId));
         }
         else
         {
-            Debug.Log("No se ha iniciado ninguna partida.");
+            Debug.Log("Juego iniciado");
         }
         
-
     }
 
-    void getTest()
+    //Obtiene información de sesión desde el servidor.
+    IEnumerator getSession(string gameId)
     {
-        StartCoroutine(GetText());
-    }
-
-    IEnumerator GetText()
-    {
-        UnityWebRequest www = UnityWebRequest.Get("http://192.168.98.131:5000/getGameSessions/4fe75d94d8734374831d82358dee2481");
+        string requestUrl = requestUrlBuilder(gameId);
+        UnityWebRequest www = UnityWebRequest.Get(requestUrl);
         yield return www.Send();
 
         if (www.isNetworkError || www.isHttpError)
@@ -73,49 +70,81 @@ public class Game : MonoBehaviour {
         }
         else
         {
-            // Show results as text
+            // Imprime información recibida.
             Debug.Log(www.downloadHandler.text);
-
-            // Or retrieve results as binary data
         }
     }
 
+    private static string requestUrlBuilder(string gameId)
+    {
+        string id = gameId;
+        string baseUrl = "http://192.168.98.131:5000/getGameSessions/";
+        string requestUrl = baseUrl + gameId;
+        return requestUrl;
+    }
+
+    //Procesa respuesta del servidor y lo convierte en JSON.
     private void ProcessJson(string jsonString)
     {
         JsonData jsonServer = JsonMapper.ToObject(jsonString);
-        
     }
 
+    //Creador de la sesión de juego
     public GameServer[] createGameSession()
     {
-        UnityWebRequest www;
-        string playersJSON = JsonHelper.ToJson(playersArray);
+        playersToJSON();
         GameServer[] gameSession = new GameServer[1];
-        gameSession[0] = new GameServer(GUIDCreator(), playersArray, "Begun", "None");
-        var gameSessionJSON = JsonHelper.ToJson(gameSession);
+        string gameSessionJSON = sessionCreator(gameSession);
         Debug.Log(gameSessionJSON);
-        www =UnityWebRequest.Post("http://192.168.98.131:5000/gameSessionInit", gameSessionJSON);
-        www.Send();
+        sessionInit(gameSessionJSON);
         return gameSession;
     }
 
+    //Crea una nueva sesión en un array y luego a JSON para ser enviado al servidor.
+    private string sessionCreator(GameServer[] gameSession)
+    {
+        gameSession[0] = new GameServer(GUIDCreator(), playersArray, "Waiting", "None");
+        var gameSessionJSON = JsonHelper.ToJson(gameSession);
+        return gameSessionJSON;
+    }
+
+    //Convertir objetos de jugadores a JSON para ser enviados al servidor.
+    private void playersToJSON()
+    {
+        string playersJSON = JsonHelper.ToJson(playersArray);
+    }
+
+    //Envia POST request para iniciar una sesión de juego en el servidor.
+    private static void sessionInit(string gameSessionJSON)
+    {
+        UnityWebRequest www = UnityWebRequest.Post("http://192.168.98.131:5000/gameSessionInit", gameSessionJSON);
+        www.Send();
+    }
+
+    //Instanciador de jugadores AI para probar el juego.
     void instantiatePlayers()
     {
+        //Instanciando jugador 1.
         enemy = GameObject.Instantiate(player1, Vector3.zero, Quaternion.identity).GetComponent<TankScript>();
         enemy.Initialize(GUIDCreator(), 100, 3, 0);
 
+        //Instanciando jugador 2-
         enemy2 = GameObject.Instantiate(player2, Vector3.zero, Quaternion.identity).GetComponent<TankScript>();
         enemy2.Initialize(GUIDCreator(), 100, -3, 0);
+
+        //Agregado jugadores al arreglo de los jugadores activos.
         playersArray.SetValue(enemy.jSONReady(),0);
-        playersArray.SetValue(enemy2.jSONReady(), 1);
+        playersArray.SetValue(enemy2.jSONReady(),1);
     }
 
+    //Crear de GUID para la sesión.
     public string GUIDCreator()
     {
         string playeriD;
         playeriD = Guid.NewGuid().ToString("N");
         return playeriD;
     }
+
     //Se inicia el juego crenado un nuevo GUID, gameState y gameWinner, ademas de los tanques.
     //No empezar el juego hasta que el estado del servidor (game session sea OK)
     //Esperar a que existan 2 tanques en el juego para que sea OK y existan en el juego.
